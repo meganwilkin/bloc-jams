180b4b9f1a4c069e8e50338d47f42baf948b8de5
Megan Wilkin <megan.wilkin@blueostrichdesign.co.uk>
2017-02-02 14:35:02 +0000

    M2C9 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -29,9 +29,11 @@
  
  //};
 
-var animatePoints = function() {
+
+
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
     
      var revealPoint = function(pointIndex){
             points[pointIndex].style.opacity = 1;
@@ -45,6 +47,18 @@ var animatePoints = function() {
      }
 };
 
-
-
-
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
